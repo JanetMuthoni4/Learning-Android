a98451125e18dcf4f127804bc5ffdee67980d7e7
Student <Student>
2016-06-11 12:13:32 +0300

    first commit

---
+++ app/src/main/java/com/jaymuthoni/learningandroid/firstFragment.java
@@ -0,0 +1,23 @@
+package com.jaymuthoni.learningandroid;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Student on 6/9/2016.
+ */
+public class  firstFragment  extends Fragment {
+    /* Type alt+insert */
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.first_layout, container, false);
+        return myView;
+    }
+}
